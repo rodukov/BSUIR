3efb41953f29b3fb42064ef0732f25825b36dc7f
138 <electroneuphoria@gmail.com>
2025-11-15 11:01:02 +0300

    APAC

--- 2cs.3sem/matlab/LAB3/max31.m
+++ 2cs.3sem/matlab/LAB3/max31.m
@@ -1,2 +1,2 @@
-function f = min31(x)
+function f = max31(x)
 f = -(5*x(1) + 5*x(2) - x(1)^2 - x(2)^2);
--- 2cs.3sem/matlab/LAB3/system_DU2.m
+++ 2cs.3sem/matlab/LAB3/system_DU2.m
@@ -1,4 +1,4 @@
 function f = system_DU2(x, y)
-f = [y(2);                          % y₁' = y₂
-     y(1) + x*exp(x)/(x^2 - 1)];    % y₂' = y₁ + x·e^x/(x²-1)
+f = [y(2);                    % y₁' = y₂
+    y(1) + x*exp(x)];         % y₂' = y₁ + x·e^x
 end
